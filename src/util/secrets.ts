213b22b78645cb480284ac6fc1f91eecaed11c0d
Jodie Struthers <jodie@shriker.ca>
2019-07-24 14:35:12 -0600

    Breaking things

---
+++ src/util/secrets.ts
@@ -0,0 +1,31 @@
+import logger from "./logger"
+import dotenv from "dotenv"
+
+logger.debug("Using .env file to supply config environment variables")
+dotenv.config({ path: ".env" })
+
+export const ENVIRONMENT = process.env.NODE_ENV
+const prod = ENVIRONMENT === "production" // Anything else is treated as 'dev'
+
+export const SESSION_SECRET = process.env["SESSION_SECRET"]
+export const MONGODB_URI = prod
+  ? process.env["MONGODB_URI"]
+  : process.env["MONGODB_URI_LOCAL"]
+
+if (!SESSION_SECRET) {
+  logger.error("No client secret. Set SESSION_SECRET environment variable.")
+  process.exit(1)
+}
+
+if (!MONGODB_URI) {
+  if (prod) {
+    logger.error(
+      "No mongo connection string. Set MONGODB_URI environment variable."
+    )
+  } else {
+    logger.error(
+      "No mongo connection string. Set MONGODB_URI_LOCAL environment variable."
+    )
+  }
+  process.exit(1)
+}
